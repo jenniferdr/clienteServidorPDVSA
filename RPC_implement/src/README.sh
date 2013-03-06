b424bbb3dfb569b6f54877584f065bb5ee01f18d
jenniferdr <jenniferdosreis@hotmail.com>
2013-03-06 19:10:36 -0430

    Agregue metodos de pedir reto y env respta

--- RPC_implement/src/README.sh
+++ RPC_implement/src/README.sh
@@ -1,6 +1,10 @@
-# Para generar los _svc.c _clnt.c y el .h que incluyen nuestros programas servidor y cliente
-#rpcgen servicioPDVSA.x
+# Hacer rpcgen servicioPDVSA.x
 
+# Instrucciones para agregar al _svc.c :
+# #include "tareasServidor.h"
+# antes de svc_run(); colocar: tareas_servidor(argc,argv);
+
+# Compilar (Hacer Makefile)
 gcc servicioPDVSA_server.c servicioPDVSA_svc.c funciones.c tareasServidor.c -pthread -o centro
 
 gcc cliente.c servicioPDVSA_clnt.c funciones.c -pthread -o bomba
@@ -8,7 +12,6 @@ gcc cliente.c servicioPDVSA_clnt.c funciones.c -pthread -o bomba
 # Para escuchar por el puerto 111
 #sudo rpcbind &
 
-# Para correr el servidor falso 
-#sudo ./centro
-
-
+# CORRER SERVIDOR Y CLIENTE
+# sudo ./centro -n C1 -cp 120000 -i 23 -t 50 -s 209 -p 10323
+# ./bomba –n Bomba -cp 200000 –i 50000 –c 200 –fc dominios
--- RPC_implement/src/servicioPDVSA_svc.c
+++ RPC_implement/src/servicioPDVSA_svc.c
@@ -22,6 +22,7 @@ serviciopdvsa_1(struct svc_req *rqstp, register SVCXPRT *transp)
 {
 	union {
 		char *pedir_gasolina_1_arg;
+		char *enviar_respuesta_1_arg;
 	} argument;
 	char *result;
 	xdrproc_t _xdr_argument, _xdr_result;
@@ -42,6 +43,18 @@ serviciopdvsa_1(struct svc_req *rqstp, register SVCXPRT *transp)
 		_xdr_argument = (xdrproc_t) xdr_void;
 		_xdr_result = (xdrproc_t) xdr_int;
 		local = (char *(*)(char *, struct svc_req *)) pedir_tiempo_1_svc;
+		break;
+
+	case PEDIR_RETO:
+		_xdr_argument = (xdrproc_t) xdr_void;
+		_xdr_result = (xdrproc_t) xdr_int;
+		local = (char *(*)(char *, struct svc_req *)) pedir_reto_1_svc;
+		break;
+
+	case ENVIAR_RESPUESTA:
+		_xdr_argument = (xdrproc_t) xdr_wrapstring;
+		_xdr_result = (xdrproc_t) xdr_int;
+		local = (char *(*)(char *, struct svc_req *)) enviar_respuesta_1_svc;
 		break;
 
 	default:
